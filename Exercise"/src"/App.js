fb0f8e6bd4faeb032ab156f34b5ed4c624e32b92
KATHANDR <86054119+KATHANDR@users.noreply.github.com>
2021-06-18 10:21:16 +0530

    Add files via upload

---
+++ Exercise"/src"/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css';
+import Progress from './Components/Progess'
+
+
+function App() {
+  return (
+<div className="App">
+     <Progress/>
+      </div>
+      );
+}
+
+export default App;
